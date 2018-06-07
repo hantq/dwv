7de3dbc19c4f0a79523d794fc240f3d4c7a9d4fb
Tianqi Han <hantq@shukun.net>
2018-06-07 16:54:10 +0800

    Add commonjs support.

--- resources/module/intro.js
+++ resources/module/intro.js
@@ -1,9 +1,16 @@
 (function(root, factory) {
-  // Browser globals (root is window)
-  root.dwv = factory(
-    root.i18next,
-    root.JSZip
-  );
+  if (typeof module === 'object' && module.exports) {
+    module.exports = factory(
+      require('i18next'),
+      require('jszip')
+    );
+  } else {
+    // Browser globals (root is window)
+    root.dwv = factory(
+      root.i18next,
+      root.JSZip
+    );
+  }
 })(this, function(i18next, JSZip) {
   // similar to what browserify does but reversed
   // https://www.contentful.com/blog/2017/01/17/the-global-object-in-javascript/
