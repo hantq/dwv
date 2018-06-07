9ce35f7bc07e0b2db210f078c8fb870f587728c2
Tianqi Han <hantq@shukun.net>
2018-06-07 18:50:20 +0800

    Remove tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,6 @@
-/* global module */
 module.exports = function(grunt) {
-    var cpTarget = grunt.option('copy-target') || '../dwv-jqmobile';
-    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        qunit: {
-            all: ['tests/index.html'],
-            options: {
-                '--web-security': 'no',
-                coverage: {
-                    disposeCollector: true,
-                    src: [ "src/**/*.js" ],
-                    instrumentedFiles: "/tmp/ivmartel/dwv",
-                    htmlReport: "build/report/coverage",
-                    lcovReport: "build/report/lcov",
-                    linesThresholdPct: 0
-                }
-            }
-        },
-        coveralls: {
-            options: {
-                // don't fail if coveralls fails
-                force: true
-            },
-            main_target: {
-                src: "build/report/lcov/lcov.info"
-            }
-        },
         concat: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
@@ -46,31 +20,10 @@ module.exports = function(grunt) {
                 }
             }
         },
-        copy: {
-            main: {
-                files: [
-                    {
-                        src: 'build/dist/<%= pkg.name %>.js',
-                        dest: cpTarget + '/node_modules/dwv/dist/<%= pkg.name %>.js'
-                    },
-                    {
-                        src: 'build/dist/<%= pkg.name %>.js',
-                        dest: cpTarget + '/node_modules/dwv/dist/<%= pkg.name %>.min.js'
-                    }
-                ]
-            }
-        },
         watch: {
-            cmd: {
-                files: ['**/*.js', '!**/node_modules/**'],
-                tasks: ['test'],
-                options: {
-                    spawn: false
-                }
-            },
             dev: {
                 files: ['**/*.js', '!**/node_modules/**'],
-                tasks: ['concat', 'copy'],
+                tasks: ['concat'],
                 options: {
                     spawn: false
                 }
@@ -90,16 +43,11 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-coveralls');
-    grunt.loadNpmTasks('grunt-qunit-istanbul');
 
     // tasks
-    grunt.registerTask('test', ['qunit']);
     grunt.registerTask('start', ['connect']);
-    grunt.registerTask('start-cmd', ['watch:cmd']);
     grunt.registerTask('dev', ['watch:dev']);
     grunt.registerTask('build', ['concat', 'uglify']);
 };
